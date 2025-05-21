84b487e2e703f5f34b474c3fb91987069b58b6da
Sanae Al Massati <sanae.almassati@outlook.com>
2025-05-21 16:21:44 +0200

    feat: :zap: Mise à jour image vélos

--- models/product.py
+++
@@ -1,8 +0,0 @@
-class ProductModel:
-    def __init__(self, id, name, description, price, stock_qty, image_url):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.price = price
-        self.stock_qty = stock_qty
-        self.image_url = image_url
