1ab54397849da5394cc50173abc006345e34b124
Sanae Al Massati <sanae.almassati@outlook.com>
2025-05-21 16:18:36 +0200

    feat: :art: Ajout images vélos populaires

---
+++ models/product.py
@@ -0,0 +1,8 @@
+class ProductModel:
+    def __init__(self, id, name, description, price, stock_qty, image_url):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.price = price
+        self.stock_qty = stock_qty
+        self.image_url = image_url
