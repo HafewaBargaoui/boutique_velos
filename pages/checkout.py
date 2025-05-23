8dfd437f304773633b0060b39272d347f3dd4668
RemiVander <rm.vanderperre@gmail.com>
2025-05-23 16:01:05 +0200

    feat: :zap: hive navbar in cart and checkout

--- pages/checkout.py
+++ pages/checkout.py
@@ -1,6 +1,14 @@
 import streamlit as st
+from utils.style import set_style
+
+
+if st.button("Retour à l'accueil"):
+    st.switch_page("main.py") 
+
 
 st.set_page_config(page_title="Commande", page_icon="✅")
+
+set_style()
 
 if "checkout_ready" not in st.session_state or not st.session_state.checkout_ready:
     st.warning("Accès non autorisé. Merci de passer par la page panier pour valider votre commande.")
