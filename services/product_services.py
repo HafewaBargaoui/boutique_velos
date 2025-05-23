5407cc91ed55f851dd7af225bf26cc4eeeff3b41
Hafawa Bargaoui <hafewa.bargaoui@gmail.com>
2025-05-23 16:50:30 +0200

    feat: :sparkles: implement product filtering by  category

--- services/product_services.py
+++ services/product_services.py
@@ -148,3 +148,39 @@ class ProductService:
         except Exception as e:
             print("Erreur lors de la récupération de l'image :", e)
             return None
+        
+    @staticmethod
+    def get_products_by_category(category_id: int):
+        try:
+            with sqlite3.connect(DB_PATH) as conn:
+                cursor = conn.cursor()
+                cursor.execute("""
+                    SELECT 
+                        p.id_product, p.name, p.description, p.price, p.stock_qty, 
+                        p.id_category, i.path
+                    FROM product p
+                    LEFT JOIN image i ON p.id_product = i.id_product
+                WHERE p.id_category = ?
+                """, (category_id,))
+                rows = cursor.fetchall()
+            if rows:
+                return [
+                    Product(
+                        id_product=row[0],
+                        name=row[1],
+                        description=row[2],
+                        price=row[3],
+                        stock_qty=row[4],
+                        id_category=row[5],
+                        path=row[6]   
+                    )
+                    for row in rows
+                ]
+            else:
+                print(f"Aucun produit dans cette categorie")
+                return []
+        except sqlite3.Error as e:
+            print("Erreur SQLite :", e)
+            return []
+        
+        
